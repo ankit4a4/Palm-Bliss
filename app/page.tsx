805ebf586cab038fe2cdc6276154c9b94c67e8db
ankit4a4 <singhankit23038@gmail.com>
2025-09-08 12:32:51 +0530

    remove gallery section from home page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,6 @@ export default function Home() {
       <AboutSection />
       <HomeWellness />
       {/* <HomeRooms /> */}
-      <GallerySection/>
       {/* <HomeYoga /> */}
       <FacilitiesGrid />
       <HomeNaturalPool />
